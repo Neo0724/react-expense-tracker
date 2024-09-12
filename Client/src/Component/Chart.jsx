affdd53105f4e773863f37c663d9c500b2f50193
github_Neo0724 <neoben007@gmail.com>
2024-09-12 21:26:22 +0800

    Added a TODO

--- Client/src/Component/Chart.jsx
+++ Client/src/Component/Chart.jsx
@@ -1,28 +1,30 @@
 import { Line } from "react-chartjs-2";
 import {
-  Chart as ChartJs,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
+    Chart as ChartJs,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
 } from "chart.js";
 import { useGlobalContext } from "../Context/useGlobalContext";
 ChartJs.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
 );
 
 export default function Chart() {
     const { income, expenses, dashboardMonth, dictForMonth, dashboardYear } = useGlobalContext();       
+    
 
+    // TODO Need to fix where for all month and all year, the graph is showing everyday instead of the sum of each month
     const filteredIncomeByMonth = () => {
         let dateHash = new Map();
 
@@ -61,47 +63,47 @@ export default function Chart() {
         return dateArr;
     };
 
-  const incomeAmount = filteredIncomeByMonth().map((item) => item[1]);
+    const incomeAmount = filteredIncomeByMonth().map((item) => item[1]);
 
-  const expensesAmount = filteredExpensesByMonth().map((item) => item[1]);
+    const expensesAmount = filteredExpensesByMonth().map((item) => item[1]);
 
-  const allDate =
-    dashboardMonth === "all"
-      ? filteredExpensesByMonth().map((item) => item[0])
-      : [
-          ...filteredExpensesByMonth().map((item) => item[0]),
-          ...filteredIncomeByMonth().map((item) => item[0]),
+    const allDate =
+        dashboardMonth === "all"
+        ? filteredExpensesByMonth().map((item) => item[0])
+        : [
+            ...filteredExpensesByMonth().map((item) => item[0]),
+            ...filteredIncomeByMonth().map((item) => item[0]),
         ];
 
-  const options = {
-    maintainAspectRatio: false,
-    responsive: true,
-  };
+    const options = {
+        maintainAspectRatio: false,
+        responsive: true,
+    };
 
-  const data = {
-    labels:
-      filteredExpensesByMonth().length === 0 &&
-      filteredIncomeByMonth().length === 0
+    const data = {
+        labels:
+        filteredExpensesByMonth().length === 0 &&
+        filteredIncomeByMonth().length === 0
         ? []
         : allDate,
 
-    datasets: [
-      {
-        label: "Income",
-        data: filteredIncomeByMonth().length === 0 ? [] : incomeAmount,
-        backgroundColor: "green",
-      },
-      {
-        label: "Expenses",
-        data: filteredExpensesByMonth().length === 0 ? [] : expensesAmount,
-        backgroundColor: "red",
-      },
-    ],
-  };
+        datasets: [
+            {
+                label: "Income",
+                data: filteredIncomeByMonth().length === 0 ? [] : incomeAmount,
+                backgroundColor: "green",
+            },
+            {
+                label: "Expenses",
+                data: filteredExpensesByMonth().length === 0 ? [] : expensesAmount,
+                backgroundColor: "red",
+            },
+        ],
+    };
 
-  return (
-    <div className="chartContainer">
-      <Line data={data} options={options} />
-    </div>
-  );
+    return (
+        <div className="chartContainer">
+        <Line data={data} options={options} />
+        </div>
+    );
 }
