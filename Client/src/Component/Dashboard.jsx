383acd9cc84781acac0a399f3fa1a0d39fdfb44b
github_Neo0724 <neoben007@gmail.com>
2024-09-13 21:55:56 +0800

    Removed unused variable

--- Client/src/Component/Dashboard.jsx
+++ Client/src/Component/Dashboard.jsx
@@ -7,7 +7,6 @@ export default function Dashboard() {
     getTotalExpensesByMonthAndYear,
     getTotalIncomeByMonthAndYear,
     getBalance,
-    getSelectedMonthHistoryTransaction,
     dashboardMonth,
     setDashboardMonth,
     dictForMonth,
